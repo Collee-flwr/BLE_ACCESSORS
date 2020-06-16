e9578578fc23afb285ce64b66e6e63c0cb68ae9b
Collee-flwr <64446124+Collee-flwr@users.noreply.github.com>
2020-06-16 12:25:23 -0500

    Update bleConnectAccessor.js

--- bleConnectAccessor.js
+++ bleConnectAccessor.js
@@ -12,7 +12,16 @@ exports.initialize = function() {
 
 function deviceConnect(){
     var device = this.get('dataIn');
-
+        evothings.ble.connectToDevice(
+              device,
+              function(device){
+                //console.log('Connected to device: ' + device.name);
+                document.querySelector('#found-devices').innerHTML = 'Connected to device: ' + device.name;
+              },
+              function(device){
+                console.log('Disconnected from device: ' + device.name);
+              },
+              function(errorCode){
+                console.log('Connect error: ' + errorCode);
+        });
 }
-
-
